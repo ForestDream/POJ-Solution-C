55f9e4e715b5654c8a32254ed3983b4dd721cbcf
Jelly Wang <wangguodong15@126.com>
2015-08-23 17:54:34 +0800

    1844 AC

---
+++ 1844.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int sum, s;
+	int i;
+	
+	scanf("%d", &s);
+	sum = 0;
+	for(i=1; i<448, sum<s || s%2 != sum%2; i++)
+		sum += i;
+	printf("%d\n", --i);
+	return 0;
+}
